646cc22b0536d52658d9d064821ccf11df8cfa4e
Wes Young <wes@barely3am.com>
2015-05-01 08:49:26 -0400

    fixes #13

---
+++ cifsdk/format/cifjson.py
@@ -0,0 +1,11 @@
+from cifsdk.format import Plugin
+import json
+
+
+class Json(Plugin):
+
+    def __init__(self, *args, **kwargs):
+        super(Json, self).__init__(*args, **kwargs)
+
+    def __repr__(self):
+        return json.dumps(self.data)
