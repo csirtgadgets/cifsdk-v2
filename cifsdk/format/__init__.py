3dc015e4a95d05339f2c1070cd597b99856a9bf3
Wes Young <wes@barely3am.com>
2015-11-18 08:19:45 -0500

    fixes #1

--- cifsdk/format/__init__.py
+++ cifsdk/format/__init__.py
@@ -1,15 +1,15 @@
-
-
 from cifsdk.format.cifjson import Json
 from cifsdk.format.cifcsv import Csv
 from cifsdk.format.table import Table
 from cifsdk.format.bro import Bro
+from cifsdk.format.cifsnort import Snort
 
 plugins = {
     'json': Json,
     'table': Table,
     'csv': Csv,
-    'bro': Bro
+    'bro': Bro,
+    'snort': Snort
 }
 
 
