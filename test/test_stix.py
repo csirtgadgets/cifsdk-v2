413a11fb8b51c1624acd573037a99c2a4bf8b7ab
Wes Young <wes@barely3am.com>
2015-12-22 12:04:34 -0500

    merging in stix bits

---
+++ test/test_stix.py
@@ -0,0 +1,44 @@
+STIX_ENABLED = True
+
+try:
+    from stix.core import STIXHeader
+    from cifsdk.format.cif_stix import Stix
+except ImportError:
+    STIX_ENABLED = False
+
+data = [
+    {
+        'observable': "example.com",
+        'provider': "me.com",
+        'tlp': "amber",
+        'confidence': "85",
+        'reporttime': '2015-01-01T00:00:00Z',
+        'tags': ['botnet', 'malware']
+    },
+    {
+        'observable': "example.com",
+        'provider': "me.com",
+        'tlp': "amber",
+        'confidence': "85",
+        'reporttime': '2015-01-01T00:00:00Z',
+        'tags': ['botnet', 'malware']
+    },
+    {
+        'observable': "example.com",
+        'provider': "me.com",
+        'tlp': "amber",
+        'confidence': "85",
+        'reporttime': '2015-01-01T00:00:00Z',
+        'tags': ['botnet', 'malware']
+    }
+]
+
+
+def test_stix():
+    if STIX_ENABLED:
+        d = Stix(data)
+        assert len(str(d)) > 2
+
+        print d
+    else:
+        print 'STIX package not installed, skipping test'
