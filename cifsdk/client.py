4911584963691d5704fe4094edd9e2f2648e4676
Wes Young <wes@barely3am.com>
2016-01-26 08:00:32 -0500

    fixes #52

--- cifsdk/client.py
+++ cifsdk/client.py
@@ -269,6 +269,7 @@ def main():
     p.add_argument('--filename', help='specify output filename [default: STDOUT]')
     p.add_argument('--ttl', help='specify number of pings to send [default: %(default)s]', default=PINGS)
     p.add_argument('--group', help='filter by group(s) (everyone,group1,group2,...)')
+    p.add_argument('--application', help='filter based on application field')
 
     # Process arguments
     args = p.parse_args()
@@ -342,6 +343,9 @@ def main():
 
         if options.get('group'):
             filters['group'] = options['group']
+
+        if options.get('application'):
+            filters['application'] = options['application']
 
         # needs to be MEG'd out.
         if options.get('last_day'):
