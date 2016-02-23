8529c4ece154efff11cdbebfc7cdbed701026b1e
Wes Young <wes@barely3am.com>
2016-02-23 10:49:07 -0500

    fixes bro / bind output

--- cifsdk/format/__init__.py
+++ cifsdk/format/__init__.py
@@ -4,12 +4,15 @@ from cifsdk.format.cifjson import Json
 from cifsdk.format.cifcsv import Csv
 from cifsdk.format.table import Table
 from cifsdk.format.bro import Bro
+from cifsdk.format.cifbind import Bind
 
 plugins = {
     'json': Json,
     'table': Table,
     'csv': Csv,
-    'bro': Bro
+    'bro': Bro,
+    #'snort': Snort,
+    'bind': Bind,
 }
 
 try:
